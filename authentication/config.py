7cb4daafdb33708b5cf1d21343bd6b2d8cec41f3
liewchooichin <liewchooichin@gmail.com>
2024-07-30 13:14:11 +0800

    remove wrong folder

--- authentication/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://cosmoslab204.documents.azure.com:443/'),
-    'master_key': os.environ.get('AUTH_ACCOUNT_KEY', ""),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'ToDoList'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
-}
