d114bd182404dee0489f7b0d816e5f4d538eb7d3
liewchooichin <liewchooichin@gmail.com>
2024-08-13 12:45:43 +0800

    ms graph quickstart

---
+++ photo/README.md
@@ -0,0 +1,5 @@
+# Using Microsoft Graph
+
+Reference
+
+[User profile photo](https://learn.microsoft.com/en-us/training/modules/msgraph-user-photo-information/3-exercise-access-user-profile-photo)
---
+++ photo/auth.js
@@ -0,0 +1,50 @@
+//MSAL configuration
+const msalConfig = {
+    auth: {
+        clientId: 'a8b2111e-28e8-46db-bf14-c09d0c5a35c6',
+        // comment out if you use a multi-tenant AAD app
+        authority: 'https://login.microsoftonline.com/2a97c710-bac6-4aa8-a5b4-0ef39f649ec4',
+        redirectUri: 'http://localhost:8080'
+    }
+};
+const msalRequest = { scopes: [] };
+function ensureScope (scope) {
+    if (!msalRequest.scopes.some((s) => s.toLowerCase() === scope.toLowerCase())) {
+        msalRequest.scopes.push(scope);
+    }
+}
+//Initialize MSAL client
+const msalClient = new msal.PublicClientApplication(msalConfig);
+
+// Log the user in
+async function signIn() {
+    const authResult = await msalClient.loginPopup(msalRequest);
+    sessionStorage.setItem('msalAccount', authResult.account.username);
+}
+//Get token from Graph
+async function getToken() {
+    let account = sessionStorage.getItem('msalAccount');
+    if (!account) {
+        throw new Error(
+            'User info cleared from session. Please sign out and sign in again.');
+    }
+    try {
+        // First, attempt to get the token silently
+        const silentRequest = {
+            scopes: msalRequest.scopes,
+            account: msalClient.getAccountByUsername(account)
+        };
+
+        const silentResult = await msalClient.acquireTokenSilent(silentRequest);
+        return silentResult.accessToken;
+    } catch (silentError) {
+        // If silent requests fails with InteractionRequiredAuthError,
+        // attempt to get the token interactively
+        if (silentError instanceof msal.InteractionRequiredAuthError) {
+            const interactiveResult = await msalClient.acquireTokenPopup(msalRequest);
+            return interactiveResult.accessToken;
+        } else {
+            throw silentError;
+        }
+    }
+}
